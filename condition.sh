1fb78cd279d6712333f894974f6e5ce274771282
Radhika <sairadhika.iiitn@gmail.com>
2025-09-22 14:20:45 +0530

    condition

--- condition.sh
+++ condition.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "Given number is less thatn 10"
+elif [ $NUMBER -eq 10]; then
+    echo "Given number is equal to 10"
 else
     echo "Given number $NUMBER is greater then 10"
 fi
