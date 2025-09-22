4c4e9be48b151936e09f92046c504c8793348d7b
Radhika <sairadhika.iiitn@gmail.com>
2025-09-22 22:56:51 +0530

    condition

--- colors.sh
+++ colors.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R ERROR :: Please run this scirpt with root privilage $N"
