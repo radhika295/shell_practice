47f30596b708b35bd1db9adeb7332a29433008cc
Radhika <sairadhika.iiitn@gmail.com>
2025-09-22 23:44:31 +0530

    condition

--- logs.sh
+++ logs.sh
@@ -14,17 +14,19 @@ fi
 LOGS_FOLDER="/var/log/shell-scripting"
 
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-echo "$SCRIPT_NAME"
+
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 
+echo "Script satrted executed at: $(date)" | tee -a $LOG_FILE
+
 VALIDATE() {
     if [ $1 -eq 0 ]; then
-         echo -e "$2 is installed successfully: $G Success $N"
+         echo -e "$2 is installed successfully: $G Success $N" | tee -a $LOG_FILE
          
     else    
-         echo -e "$2 is not installed it is failed: $R Failed $N"
+         echo -e "$2 is not installed it is failed: $R Failed $N" | tee -a $LOG_FILE
          exit 1
     fi
 }
@@ -34,12 +36,12 @@ if [ $? -ne 0 ]; then
    dnf install mysql -y &>>$LOG_FILE
    VALIDATE $? "MYSQL"
 else
-    echo -e "MySql already existed...$G Skipped $N"
+    echo -e "MySql already existed...$G Skipped $N" | tee -a $LOG_FILE
 fi
 dnf list installed nginx &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "Nginx"
 else
-    echo -e "Nginx already existed...$G Skipped $N"
+    echo -e "Nginx already existed...$G Skipped $N" | tee -a $LOG_FILE
 fi
