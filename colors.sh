0073ce2935f5161ad9f106d50112020bf5b32aab
Radhika <sairadhika.iiitn@gmail.com>
2025-09-22 22:53:06 +0530

    condition

---
+++ colors.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR :: Please run this scirpt with root privilage $N"
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+         echo -e "$2 is installed successfully: $G Success $N"
+         
+    else    
+         echo "$2 is not installed it is failed: $R Failed $N"
+         exit 1
+    fi
+}
+dnf list installed mysql 
+if [$? -ne 0]; then 
+   dnf install mysql -y
+   VALIDATE $? "MYSQL"
+else
+    echo "MySql already existed...$G Skipped $N"
+dnf list installed nginx
+if [$? -ne 0]; then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo "Nginx already existed...$G Skipped $N"
