87b76c64a4b1ac789156c89b6ed10c4367d8aeb0
Radhika <sairadhika.iiitn@gmail.com>
2025-09-23 14:31:57 +0530

    condition

--- loops.sh
+++ loops.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 
-for i in {1..20}
-do 
-echo $i 
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR :: Please run this scirpt with root privilage $N"
+    exit 1
+fi
+LOGS_FOLDER="/var/log/shell-scripting"
+
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script satrted executed at: $(date)" | tee -a $LOG_FILE
+
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+         echo -e "$2 is installed successfully: $G Success $N" | tee -a $LOG_FILE
+         
+    else    
+         echo -e "$2 is not installed it is failed: $R Failed $N" | tee -a $LOG_FILE
+         exit 1
+    fi
+}
+
+# $@ ---all arguments
+
+for package in $@
+do
+    echo "$package"
 done
