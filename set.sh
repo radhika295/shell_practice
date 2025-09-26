4787675a8d93aa6b00799f5d49e35516e340709a
Radhika <sairadhika.iiitn@gmail.com>
2025-09-26 17:09:07 +0530

    roboshop

--- set.sh
+++ set.sh
@@ -6,7 +6,7 @@ error(){
 
    echo 'there is an error'
 }
-trap 'echo "there is an error in $LINENO, command is : $BASh_COMMAND"' ERR
+trap 'echo "there is an error in $LINENO, command is : $BASh_COMMAND"'  ERR
 echo "HEllo"
 ahdkghsdlkfj
 echo "above line is an error"
